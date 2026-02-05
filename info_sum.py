c5e579bdaeb4bdc88fe345bbb478c740964d2c5c
Jason Chen <jnchen12a@gmail.com>
2026-02-05 16:26:35 -0500

    initial code

---
+++ info_sum.py
@@ -0,0 +1,9 @@
+'''
+Function that takes in text and returns summarized notes, with bulletpoints and appropriate title and sections
+'''
+
+def summarize(t: str) -> str:
+    return  ''
+
+if __name__ == '__main__':
+    pass
